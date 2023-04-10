21e08540b736858782051b7d0f05dc500aa7b962
bhopusinha <108219196+bhopusinha@users.noreply.github.com>
2023-04-11 00:54:43 +0530

    changed

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -6,7 +6,7 @@ import DetailsBanner from './detailsBanner/DetailsBanner'
 import Cast from './cast/Cast'
 import VideosSection from './videosSection/VideosSection'
 import Similar from './carousels/Similar'
-import Recommendation from './carousels/recommendation'
+import Recommendation from './carousels/Recommendation'
 const Details = () => {
   const {mediaType,id}=useParams();
   const {data,loading}=useFetch(`/${mediaType}/${id}/videos`)
